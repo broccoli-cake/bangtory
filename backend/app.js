41024acdceedd82ed17c2c74f43bc8e3100cd72d
msy <msy11800@gmail.com>
2025-04-24 16:42:43 +0900

    [BE] test: add.js 수정

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 // app.js
 
-require('dotenv').config(); // 맨 위에 추가
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000; // .env에서 불러옴
+const userRoutes = require("./routes/userRoutes");
 
-app.get('/', (req, res) => {
-  res.send('방토리 서버가 잘 작동 중입니다!');
-});
+app.use(express.json());
+app.use("/api", userRoutes); // /api/users 요청 가능
 
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+    //  require('dotenv').config(); // 맨 위에 추가
+    //  const express = require('express');
+    //  const app = express();
+    //  const PORT = process.env.PORT || 3000; // .env에서 불러옴
+
+    //  app.get('/', (req, res) => {
+    //    res.send('방토리 서버가 잘 작동 중입니다!');
+    //  });
+
+    //  app.listen(PORT, () => {
+    //    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다`);
+    //  });
