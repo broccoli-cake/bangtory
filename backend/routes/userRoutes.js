5a6ad343e8823ad111e8111acdc6d4ac4634dad7
broccoli-cake <gchk0905@gmail.com>
2025-04-28 15:32:03 +0900

    [BE] chore: 기본 디렉토리 및 OAuth 구조 설정 및 수정

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-const { auth } = require("../middlewares/authMiddleware");
-
-router.get("/users", auth, userController.getUsers);
-
-module.exports = router;
