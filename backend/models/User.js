293bfab568cbff84b9c975305b120d3d18738e8f
msy <msy11800@gmail.com>
2025-05-10 03:36:08 +0900

    [BE] chore: User 모델에서 provider enum에 'naver'를 추가

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   provider: {
     type: String,
-    enum: ['kakao', 'google'],
+    enum: ['kakao', 'google', 'naver'],
     required: true
   },
   providerId: {
