347fe71cc26f8bce0cb630833a3404d692ce963d
msy <msy11800@gmail.com>
2025-05-10 03:06:44 +0900

    [BE] feat: passport 초기화 파일에 네이버 전략 추가

--- backend/config/passport/index.js
+++ backend/config/passport/index.js
@@ -27,5 +27,8 @@ require('./kakaoStrategy');
 // 구글 전략 설정 파일 불러오기
 require('./googleStrategy');
 
+// 네이버 전략 설정 파일 불러오기
+require('./naverStrategy');
+
 // 설정된 passport 객체를 외부에 내보내기
 module.exports = passport;
