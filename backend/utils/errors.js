77cc23d23565eea4c88de90f79e9b020f6ad3345
msy <msy11800@gmail.com>
2025-05-27 22:41:48 +0900

    [BE] feat: 예약관련 에러 추가

--- backend/utils/errors.js
+++ backend/utils/errors.js
@@ -14,13 +14,23 @@ module.exports = {
 
 
 class ReservationError extends Error {
-  constructor(message, statusCode = 500) {
+  constructor(message, statusCode = 400) {
     super(message);
     this.name = 'ReservationError';
     this.statusCode = statusCode;
   }
 }
 
+class ValidationError extends Error {
+  constructor(message, details = []) {
+    super(message);
+    this.name = 'ValidationError';
+    this.statusCode = 400;
+    this.details = details;
+  }
+}
+
 module.exports = {
-  ReservationError
+  ReservationError,
+  ValidationError
 };
